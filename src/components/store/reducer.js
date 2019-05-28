5ca4428c62a980dae4019fc8bba0a88d0e10e48d
Tony Kieling <tony.kieling@gmail.com>
2019-05-27 19:41:46 -0700

    changes on index.js to set redux were made

--- src/components/store/reducer.js
+++ src/components/store/reducer.js
@@ -7,16 +7,18 @@ const reducer = (state = initialState, action) => {
   const newState = {... state}
   if (action.type === "LOGIN") {
     newState = {
-      email = action.value.email,
-      typeUser = action.value.typeUser
+      email: action.value.email,
+      typeUser: action.value.typeUser
     }
 
   } else if (action.type === "LOGOUT") {
     newState = {
-      email = "",
-      typeUser = ""
+      email: "",
+      typeUser: ""
     }
   }
 
   return newState
 }
+
+export default reducer
