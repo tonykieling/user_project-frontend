3b6e25f23d2c3212219b03852c68b9e7930a9e45
Tony Kieling <tony.kieling@gmail.com>
2019-06-07 13:54:28 -0700

    Navega.js was cleaned and it's working

--- src/components/Navega.js
+++ src/components/Navega.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react'
 import {Navbar, Nav, Button} from 'react-bootstrap'
-//import ReactBootstrap, {Navbar} from 'react-bootstrap'
 import { connect } from 'react-redux'
-// import { Redirect } from "react-router-dom";
-// import store from '../components/store/store.js'
-
 
 class Navega extends Component {
-  // constructor(props){
-  //   super(props);
-  // }
 
   logout = () => {
-    // 
-    // insert confirmation popup
-    //alert message
-    // console.log("BEFORE logout this.props== ", this.props)
     this.props.noUser() 
-    // console.log("logout")
-    // console.log("AFTER logout this.props== ", this.props)
-    // return <Redirect to="/user" />
   }
 
   loggedHeader = () => {
@@ -38,18 +24,14 @@ class Navega extends Component {
          <Nav.Link href="/">Home</Nav.Link>
       </Nav>
     )
-  }  // ==================  end of NOT LOGGED  ================
+  }
 
   render() {
-    // console.log("Nav-props: ", this.props.userEmail);
-
     return (
-
         <Navbar bg="primary" variant="dark">
             <Navbar.Brand href="/">MyProjectLogin</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              {console.log("this.props.userEmail:: ", this.props.userEmail)}
                 {this.props.userEmail ?
                   (this.loggedHeader()) :
                   (this.notLoggedHeader())
@@ -57,11 +39,10 @@ class Navega extends Component {
             </Navbar.Collapse>
         </Navbar>
     )
-  } // ==================  end of RENDER  ================
-} // ==================  end of COMPONENT  ================
+  }
+}
 
 const mapStateToProps = store => {
-  // console.log("store:: ", store)
   return {
     userEmail: store.email
   }
@@ -70,7 +51,6 @@ const mapStateToProps = store => {
 const mapDispatchToProps = dispatch => {
   return {
     noUser: () => dispatch({type:"LOGOUT"})
-    // noUser: () => dispatch({type:"NO_USER", data: ""})
   }
 }
 
