d52a85595285d59e058f28b192a2f37d4fcc78fc
Tony Kieling <tony.kieling@gmail.com>
2019-06-07 17:33:38 -0700

    check user in UserPage was changed and it's working

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,37 +1,27 @@
 import React, { Component } from 'react'
-// import store from './store/store.js'
 import {Button, Card} from 'react-bootstrap'
 import { connect } from 'react-redux'
 
-
 class UserPage extends Component {
-
   render() {
-
-    console.log('get props TYPE >>> ', this.props.storeType)
-    console.log('get props NAME >>> ', this.props.storeName)
-
     return (
       <div className="moldura">
         <h1>User Page</h1>
-
         <Card>
           <Card.Body>
             <Card.Title>Name: { this.props.storeName }</Card.Title>
               <div className="cardDetails">
                 <div className="cardLeft">
-                    <img src={require("../img/userphoto.png")} alt="user" />
+                  <img src={require("../img/userphoto.png")} alt="user" />
                 </div>
                 <div className="cardRight">
-                    <Card.Text>Status: Active</Card.Text>
-                    <Card.Text>Type: { eval(this.props.storeType) ? "Admin" : "Normal" }</Card.Text>
-                    {console.log("TYPEOF::::::::::::::::: ", eval(this.props.storeType))}
-                    <Button variant="secondary" href="/">Close</Button>
+                  <Card.Text>Status: Active</Card.Text>
+                  <Card.Text>Type: { this.props.storeType === "true" ? "Admin" : "Normal" }</Card.Text>
+                  <Button variant="secondary" href="/">Close</Button>
                 </div>
               </div>
           </Card.Body>
         </Card>
-
       </div>
     )
   }
@@ -43,6 +33,5 @@ const mapStateToProps = store => {
     storeType: store.userAdmin
   }
 }
-
 
 export default connect(mapStateToProps, null)(UserPage)
