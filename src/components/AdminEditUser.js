a99b6f0386b5317d5d67d409f255e1f556336f5f
Tony Kieling <tony.kieling@gmail.com>
2019-06-30 19:08:36 -0700

    radio button is working on adminchangeuser, missing send data and update on backend

--- src/components/AdminEditUser.js
+++ src/components/AdminEditUser.js
@@ -27,14 +27,14 @@ class AdminEditUser extends Component {
   clearMessage = () => {
     setTimeout(() => {
       this.setState({
-        errorMsg: "",
-        flagMsg: "",
-        disableEditData: true,
-        errorMsgPassword: "",
-        disableEditPassword: true,
-        newPassword: "",
-        currentPassword: "",
-        confNewPassword: ""
+        errorMsg            : "",
+        flagMsg             : "",
+        disableEditData     : true,
+        errorMsgPassword    : "",
+        disableEditPassword : true,
+        newPassword         : "",
+        currentPassword     : "",
+        confNewPassword     : ""
       })
     }, 5000);
   }
@@ -43,12 +43,12 @@ class AdminEditUser extends Component {
     event.preventDefault();
     if (event.target.name === "changePassword")
       this.setState({ 
-        disableEditPassword: !this.state.disableEditPassword,
-        disableEditData: true });
+        disableEditPassword : !this.state.disableEditPassword,
+        disableEditData     : true });
     else if (event.target.name === "editData") {
       this.setState({ 
-        disableEditData: !this.state.disableEditData,
-        disableEditPassword: true });
+        disableEditData     : !this.state.disableEditData,
+        disableEditPassword : true });
     }
   }
 
@@ -62,20 +62,20 @@ class AdminEditUser extends Component {
        (this.state.currentPassword !== "") &&
        (this.state.newPassword !== "")) {
         bodyData = JSON.stringify({
-          currentPassword: this.state.currentPassword,
-          newPassword: this.state.newPassword,
-          actualEmail: this.props.storeEmail
+          currentPassword : this.state.currentPassword,
+          newPassword     : this.state.newPassword,
+          actualEmail     : this.props.storeEmail
         });
       } else {
         if (this.state.currentPassword === "" || this.state.newPassword === "" || this.state.confNewPassword === "")
           this.setState({
-            errorMsgPassword: "Password cannot be empty!",
-            flagMsg: "NOK"
+            errorMsgPassword  : "Password cannot be empty!",
+            flagMsg           : "NOK"
           })
         else if (this.state.newPassword !== this.state.confNewPassword)
           this.setState({
-            errorMsgPassword: "Diff passwords!",
-            flagMsg: "NOK"
+            errorMsgPassword  : "Diff passwords!",
+            flagMsg           : "NOK"
           })
 
         this.clearMessage();
@@ -83,14 +83,35 @@ class AdminEditUser extends Component {
       }
     } else if ((this.state.name !== this.props.storeName) || (this.state.email !== this.props.storeEmail)) {
       bodyData =  JSON.stringify({
-        actualEmail: this.props.storeEmail,
-        name: this.state.name,
-        email: this.state.email
+        actualEmail : this.props.storeEmail,
+        name        : this.state.name,
+        email       : this.state.email,
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+        // pass data to update user's data
+        // should consider adminEmail because it should record who is changing
+        // also change in the updateUser's method to recordLog the admin email
+        ///////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
       })
     } else {
       this.setState({
-        errorMsg: "Same data. No changes performed",
-        flagMsg: "NOK"
+        errorMsg  : "Same data. No changes performed",
+        flagMsg   : "NOK"
       })
       this.clearMessage();
       return;
@@ -98,49 +119,49 @@ class AdminEditUser extends Component {
     
     const url = "http://localhost:3333/user";
       fetch( url, {  
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: bodyData
+        method  : "PUT",
+        headers : { "Content-Type": "application/json" },
+        body    : bodyData
       })
         .then(response => response.json())
         .then((resJSON) => {
           if ('name' in resJSON){
             const user = {
-              id: resJSON.id,
-              name: resJSON.name,
-              email: resJSON.email,
-              userAdmin: resJSON.user_admin,
-              userActive: resJSON.user_active
+              id          : resJSON.id,
+              name        : resJSON.name,
+              email       : resJSON.email,
+              userAdmin   : resJSON.user_admin,
+              userActive  : resJSON.user_active
             }; 
             this.props.dispatchLogin({user});
             if ('email' in JSON.parse(bodyData))
               this.setState({
-                errorMsg: "Data updated successfully!",
-                flagMsg: "OK"});
+                errorMsg  : "Data updated successfully!",
+                flagMsg   : "OK"});
             else
               this.setState({
-                errorMsgPassword: "Password has been changed successfuly!",
-                flagMsg: "OK"});
+                errorMsgPassword : "Password has been changed successfuly!",
+                flagMsg          : "OK"});
             this.clearMessage();
           }
           else if ('message' in resJSON){
             this.setState({
-              errorMsg: resJSON.message,
-              flagMsg: "NOK"});
+              errorMsg  : resJSON.message,
+              flagMsg   : "NOK"});
             this.clearMessage();
           }
           else if ('messagePassword' in resJSON){
             this.setState({
-              errorMsgPassword: resJSON.messagePassword,
-              flagMsg: "NOK"});
+              errorMsgPassword  : resJSON.messagePassword,
+              flagMsg           : "NOK"});
             this.clearMessage();
           }          
         })
         .catch((error) => {
           console.error(error);
           this.setState({
-            errorMsg: error.message,
-            flagMsg: "NOK"});
+            errorMsg  : error.message,
+            flagMsg   : "NOK"});
           this.clearMessage();
         })
     
@@ -166,25 +187,12 @@ class AdminEditUser extends Component {
 
   handleUserProperty = event => {
     event.preventDefault();
-    console.log("event.target.value", event.target.name, event.target.value)
-    // if (event.target.name === "userAdmin")
-    //   this.setState({ userAdmin: event.target.value });
-    // else if (event.target.name === "userActive")
-    //   this.setState( { userActive: event.target.value })
-      this.setState(
-        { [event.target.name]: !!(event.target.value)
-      });
-      console.log("===", this.state);
+    const tf = (event.target.value === "true") ? true : false;
+    this.setState( { [event.target.name]: tf });
   }
 
 
-
-
-
-
-
   render() {
-    console.log("this.state", this.state)
     return (
       <div className="moldura">
         <h1>Admin Edit User's data</h1>
@@ -196,13 +204,13 @@ class AdminEditUser extends Component {
               <Form.Label column sm={2}>Name</Form.Label>
               <Col sm={10}>
                 <Form.Control
-                  type="text"
-                  placeholder="User's name"
-                  name="name"
-                  disabled={this.state.disableEditData}
-                  onChange={this.handleChange}
-                  onKeyPress={this.handles}
-                  value={this.state.name}/>
+                  type        = "text"
+                  placeholder = "User's name"
+                  name        = "name"
+                  disabled    = {this.state.disableEditData}
+                  onChange    = {this.handleChange}
+                  onKeyPress  = {this.handles}
+                  value       = {this.state.name}/>
               </Col>
             </Form.Group>
 
@@ -210,34 +218,31 @@ class AdminEditUser extends Component {
               <Form.Label column sm={2}>Email</Form.Label>
               <Col sm={10}>
                 <Form.Control
-                  type="email"
-                  disabled={this.state.disableEditData}
-                  placeholder="Users' email"
-                  name="email"
-                  onChange={this.handleChange}
-                  onKeyPress={this.handles}
-                  value={this.state.email}/>
+                  type        = "email"
+                  disabled    = {this.state.disableEditData}
+                  placeholder = "Users' email"
+                  name        = "email"
+                  onChange    = {this.handleChange}
+                  onKeyPress  = {this.handles}
+                  value       = {this.state.email}/>
               </Col>
             </Form.Group>
-
-
-
 
             <FormGroup as={Row}>
               <Form.Label column sm={2}> Admin </Form.Label>
               <Col sm={10}>
                 <Button 
-                  onClick=    { this.handleUserProperty } 
-                  value=      { true }
-                  name=       "userAdmin"
-                  variant=    { !!(this.state.userAdmin) ? "success" : "secondary" }
-                  disabled=   { this.state.disableEditData } > Yes </Button>
+                  onClick   = { this.handleUserProperty } 
+                  value     = "true"
+                  name      = "userAdmin"
+                  variant   = { !!(this.state.userAdmin) ? "success" : "secondary" }
+                  disabled  = { this.state.disableEditData } > Yes </Button>
                 <Button
-                  onClick=    { this.handleUserProperty } 
-                  value=      { false }
-                  name=       "userAdmin"
-                  variant=    { !(this.state.userAdmin) ? "success" : "secondary" }
-                  disabled=   { this.state.disableEditData } > No  </Button>
+                  onClick   = { this.handleUserProperty } 
+                  value     = "false"
+                  name      = "userAdmin"
+                  variant   = { !(this.state.userAdmin) ? "success" : "secondary" }
+                  disabled  = { this.state.disableEditData } > No  </Button>
               </Col>
             </FormGroup>
 
@@ -245,39 +250,31 @@ class AdminEditUser extends Component {
               <Form.Label column sm={2}> Active </Form.Label>
               <Col sm={10}>
                 <Button 
-                  onClick=    { this.handleUserProperty } 
-                  value=      "true"
-                  name=       "userActive"
-                  variant=    { this.state.userActive ? "success" : "secondary" }
-                  disabled=   { this.state.disableEditData }                
+                  onClick   = { this.handleUserProperty } 
+                  value     = "true"
+                  name      = "userActive"
+                  variant   = { this.state.userActive ? "success" : "secondary" }
+                  disabled  = { this.state.disableEditData }                
                   > Yes </Button>
                 <Button 
-                  onClick=    { this.handleUserProperty } 
-                  value=      "false"
-                  name=       "userActive"
-                  variant=    { !this.state.userActive ? "success" : "secondary" }
-                  disabled=   { this.state.disableEditData }
+                  onClick   = { this.handleUserProperty } 
+                  value     = "false"
+                  name      = "userActive"
+                  variant   = { !this.state.userActive ? "success" : "secondary" }
+                  disabled  = { this.state.disableEditData }
                 > No  </Button>
               </Col>
             </FormGroup>
-
-            
-
-
-
-
-
-
 
             <div>
               <Button variant="primary" type="submit" onClick={this.handleEdit} name="editData">
                 {this.state.disableEditData ? "Edit Data" : "Cancel Edit"}
               </Button>
               <Button 
-                variant="success" 
-                type="submit" 
-                onClick={this.handleSave}
-                disabled={this.state.disableEditData} >
+                variant = "success" 
+                type    = "submit" 
+                onClick = {this.handleSave}
+                disabled= {this.state.disableEditData} >
                 Save
               </Button>
               <span id={(this.state.flagMsg === "OK") ? "errorMsgBlue" : "errorMsgRed"}>{ this.state.errorMsg }</span>
@@ -293,13 +290,13 @@ class AdminEditUser extends Component {
             <Form.Label column sm={2}>Current</Form.Label>
               <Col sm={10}>
                 <Form.Control
-                  type="password"
-                  placeholder="Current password"
-                  name="currentPassword"
-                  disabled={this.state.disableEditPassword}
-                  onChange={this.handleChange}
-                  onKeyPress={this.handles}
-                  value={this.state.currentPassword}
+                  type        = "password"
+                  placeholder = "Current password"
+                  name        = "currentPassword"
+                  disabled    = {this.state.disableEditPassword}
+                  onChange    = {this.handleChange}
+                  onKeyPress  = {this.handles}
+                  value       = {this.state.currentPassword}
                 />
               </Col>
             </Form.Group>
@@ -308,13 +305,13 @@ class AdminEditUser extends Component {
             <Form.Label column sm={2}>New</Form.Label>
             <Col sm={10}>
               <Form.Control
-                type="password"
-                placeholder="New password"
-                name="newPassword"
-                disabled={this.state.disableEditPassword}
-                onChange={this.handleChange}
-                onKeyPress={this.handles}
-                value={this.state.newPassword}
+                type        = "password"
+                placeholder = "New password"
+                name        = "newPassword"
+                disabled    = {this.state.disableEditPassword}
+                onChange    = {this.handleChange}
+                onKeyPress  = {this.handles}
+                value       = {this.state.newPassword}
               />
             </Col>
           </Form.Group>
@@ -323,13 +320,13 @@ class AdminEditUser extends Component {
             <Form.Label column sm={2}>Confirm</Form.Label>
             <Col sm={10}>
               <Form.Control
-                type="password"
-                disabled={this.state.disableEditPassword}
-                placeholder="Confirm new password"
-                name="confNewPassword"
-                onChange={this.handleChange}
-                onKeyPress={this.handles}
-                value={this.state.confNewPassword}
+                type        = "password"
+                disabled    = {this.state.disableEditPassword}
+                placeholder = "Confirm new password"
+                name        = "confNewPassword"
+                onChange    = {this.handleChange}
+                onKeyPress  = {this.handles}
+                value       = {this.state.confNewPassword}
               />
             </Col>
           </Form.Group>
@@ -352,11 +349,11 @@ class AdminEditUser extends Component {
 
 const mapStateToProps = store => {
   return {
-    storeId: store.userToBeChangedId,
-    storeName: store.userToBeChangedName,
-    storeEmail: store.userToBeChangedEmail,
-    storeUserAdmin: store.userToBeChangedUserAdmin,
-    storeUserActive: store.userToBeChangedUserActive
+    storeId         : store.userToBeChangedId,
+    storeName       : store.userToBeChangedName,
+    storeEmail      : store.userToBeChangedEmail,
+    storeUserAdmin  : store.userToBeChangedUserAdmin,
+    storeUserActive : store.userToBeChangedUserActive
   }
 };
 
